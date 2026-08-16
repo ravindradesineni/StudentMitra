6fda7836339819745e4a7f583bc9133e291dccf4
HP <HP@LAPTOP-BU3DD4IV>
2026-08-16 16:36:43 +0530

    update dbconnection

--- StudentManagementSystem/src/main/java/com/sms/util/DBConnection.java
+++ StudentManagementSystem/src/main/java/com/sms/util/DBConnection.java
@@ -6,9 +6,12 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/student_management_system";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
+//    private static final String URL = "jdbc:mysql://localhost:3306/student_management_system";
+//    private static final String USERNAME = "root";
+//    private static final String PASSWORD = "root";
+    private static final String URL = System.getenv("DB_URL");
+	private static final String USERNAME = System.getenv("DB_USERNAME");
+	private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() {
 
